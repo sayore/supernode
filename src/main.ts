2be6cad3367468a1346d9165dda2c635081db4cf
edenprojectde <bkrippner@yahoo.de>
2021-10-22 20:17:02 +0200

    Moved output to root of the package to make the path more consistent.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ export * as String from './String/mod'
 export * as Game from './Game/mod'
 export * as Math from './Math/mod'
 export * as Packages from './Packages/mod'
-//export * as Server from './Server/mod'
 export * as Discord from './Discord/mod'
 export * as Express from './Express/mod'
+
+//export * as Server from './Server/mod' 

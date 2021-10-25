db2eb48e7f8e69e3a511c9b2d26639a5b6546e7a
edenprojectde <bkrippner@yahoo.de>
2021-10-25 16:06:51 +0200

    Added more meta and an ApplicationCollection so more than one App can be registred.

--- src/Base/Application.ts
+++ src/Base/Application.ts
@@ -1,4 +1,42 @@
 export interface Application {
-    init() : void
-    run() : void
+    /**
+     * Define a function to run on error event.
+     */
+    error?(eventdata?:any) : void
+    /**
+     * Define a function to run on exit event.
+     */
+    exit?(eventdata?:any) : void
+    /**
+     * Define a function to run on init event.
+     */
+    init?(eventdata?:any) : void
+    /**
+     * Define a function to run permanently
+     */
+    run(eventdata?:any) : void
+    /**
+     * Set the mode the Application shall be run on the sevServer
+     */
+    typeOfApplication? : TypeOfApplication
+    needsSafeMode? : SafetyMode
+    meta? : object
+}
+
+export interface ApplicationCollection {
+    applications:Application[]
+    meta? : object
+}
+
+enum TypeOfApplication {
+    Webserver,
+    BackgroundProcess,
+    Database
+}
+
+enum SafetyMode {
+    NeedsCatch,
+    Safe,
+    OnceNeedsCatch,
+    Once
 }

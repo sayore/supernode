139f6b3f6659604ceff03e73bdfa1bfff4b7411d
edenprojectde <bkrippner@yahoo.de>
2021-10-28 12:17:36 +0200

    Fixed incoherence between supernode and this project.

--- Base/ApplicationCollection.js
+++ Base/ApplicationCollection.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ApplicationCollection = void 0;
 const Application_1 = require("./Application");
@@ -25,7 +34,7 @@ class ApplicationCollection {
         });
     }
     run(eventdata) {
-        this.applications.forEach((app) => {
+        this.applications.forEach((app) => __awaiter(this, void 0, void 0, function* () {
             if (app.Type == Application_1.TypeOfApplication.Express)
                 ExpressApplicationHandler_1.ExpressApplicationHandler.registerSubApp(app);
             else
@@ -36,7 +45,7 @@ class ApplicationCollection {
                     app.error(e);
                     app.restart();
                 }
-        });
+        }));
         if (!!ExpressApplicationHandler_1.ExpressApplicationHandler)
             ExpressApplicationHandler_1.ExpressApplicationHandler.run();
     }
